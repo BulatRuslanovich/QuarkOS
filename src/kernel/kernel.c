7db12e456970d91502fdb489ce5c33f788abae4d
Bulat <bbr@getcrm.ru>
2025-04-08 22:28:33 +0300

    feat: add username and password

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -8,45 +8,59 @@
 s32 kmain() {
     clear_screen();
     print_rick_and_morty();
-    printf("QuarkOS v1.0\n");
-    printf("Memory: %d MB\n", 1024);
-    printf("Address: %x\n", 0x100000);
-    printf("> ");
+    printf("Welcome to QuarkOS v1.0\n");
 
-    char input[50];
+    char username[50];
+    char password[50];
+    char* os_name = "quark";
 
     while (1) {
-        scanf(input, sizeof(input));
+        printf("Username: ");
+        scanf(username, sizeof(username));
+        printf("Password: ");
+        scanf(password, sizeof(password));
 
-        // printf("You entered: ");
-        // printf(input);
+        if (strcmp(username, "root") == 0 && strcmp(password, "root") == 0) {
+            break;
+        } else {
+            colored_print(0x04 ,"Wrong Username or Password\n");
+        }
+    }
 
-        if (strcmp(input, "q") == 0) {
+    char command[50];
+
+    printf("%s@%s:~$ ", username, os_name);
+
+    while (1) {
+        scanf(command, sizeof(command));
+
+        if (strcmp(command, "q") == 0) {
             clear_screen();
             printf("Shutting down...");
             // asm volatile("hlt");
             power_off();
             break;
-        } else if (!strcmp(input, "cow")) {
+        }
+
+        if (!strcmp(command, "cow")) {
             printf("\n");
             print_cow();
-        } else if (!strcmp(input, "clear")) {
+        } else if (!strcmp(command, "clear")) {
             clear_screen();
-        } else if (!strcmp(input, "rimo")) {
+        } else if (!strcmp(command, "rimo")) {
             print_rick_and_morty();
-
-        } else if (!strcmp(input, "help")) {
+        } else if (!strcmp(command, "whoami")) {
+            printf(username);
+        } else if (!strcmp(command, "help")) {
             clear_screen();
             colored_print(0x0F, " help  | Show this menu\n");
             colored_print(0x0F, " clear | Clear screen\n");
             colored_print(0x0F, " cow   | Show ASCII art cow\n");
             colored_print(0x0F, " rimo  | Rick and Morty art\n");
             colored_print(0x0F, " q     | Shutdown system\n");
-        } else {
-            colored_print(0x04, "Invalid input!");
         }
 
-        printf("\n> ");
+        printf("%s@%s:~$ ", username, os_name);
     }
 
     return 0;
